1e059cd83bf591d77934b7dbbd3d8f7c17b49e12
bahodir373 <zteqwertyuiop672@gmail.com>
2025-01-08 21:38:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/count-words', (req, res) => {
+    const { text } = req.body;
+
+    if (!text || typeof text !== 'string') {
+        return res.status(400).json({ error: 'Iltimos, toʻgʻri matn yuboring.' });
+    }
+
+    const wordCount = text.trim().split(' ').filter(word => word !== '').length;
+		console.log(wordCount)
+
+    res.json({ wordCount });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+});
